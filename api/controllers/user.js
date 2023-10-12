15973381e744c056da6bebc945ada9cc486edaf5
arthurzop <medeirosartur48@gmail.com>
2023-10-12 16:56:05 -0300

    finalizado

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,4 +8,52 @@ export const getUsers = (_, res) => {
 
         return res.status(200).json(data);
     })
+};
+
+//exportando funcao de adicionar usuario
+export const addUser = (req, res) => {
+    const q = "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES (?)"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento
+    ];
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("usuario criado com sucesso.")
+    })
+};
+
+//exportando funcao de atualizar usuario
+export const updateUser = (req, res) =>{
+    const q = "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ]
+
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("usuario atualizado com sucesso.")
+    })
+}
+
+//exportando funcao de adicionar usuario
+export const deleteUser = (req, res) => {
+    const q = 'DELETE FROM usuarios WHERE id = ?'
+
+    db.query(q, [req.params.id], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("usuario deletado com sucesso.")
+    })
+
 }
